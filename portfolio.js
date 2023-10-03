602daa6a21c4b67c51cea1b35da01bf30d35e324
Neil Llona <124287010+neilllona@users.noreply.github.com>
2023-10-03 12:01:20 -0400

    css update

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 let displayNeil = function(){
-    text.innerHTML = ("<b>Neil Llona</br>neil.llona4@gmail.com</br>(908)-692-3347</b>");
+    text.innerHTML = (`<b>Neil Llona</br>neil.llona4@gmail.com</br><a href=https://github.com/neilllona target="_blank">GitHub</a></br><a href=https://www.linkedin.com/in/neil-llona target="_blank">Linkedin</a></br>(908)-692-3347</b>`);
 }
 
 let neil = document.getElementById("neil");
