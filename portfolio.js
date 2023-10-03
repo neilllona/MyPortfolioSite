ef2be13b558dff101185b582977a73e5c9347e88
Neil Llona <124287010+neilllona@users.noreply.github.com>
2023-10-03 11:25:31 -0400

    It's ya boy

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+let displayNeil = function(){
+    text.innerHTML = ("<b>Neil Llona</br>neil.llona4@gmail.com</br>(908)-692-3347</b>");
+}
+
+let neil = document.getElementById("neil");
+let text = document.getElementById("text");
+neil.addEventListener("click", displayNeil);
+
